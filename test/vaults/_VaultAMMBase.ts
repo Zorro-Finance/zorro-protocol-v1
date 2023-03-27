4d2b3a83d9328ba0430db767337320b397143e43
Deltakilomilo <deltakilomilo@zorro.finance>
2023-03-27 13:01:02 -0400

    vault tests deployer

--- test/vaults/_VaultAMMBase.ts
+++ test/vaults/_VaultAMMBase.ts
@@ -3,6 +3,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
 import {deploymentArgs} from '../../helpers/deployments';
+import { zeroAddress } from "../../helpers/constants";
 
 describe('VaultAMMBase', () => {
     async function deployVaultAMMBaseFixture() {
@@ -21,17 +22,54 @@ describe('VaultAMMBase', () => {
     }
 
     describe('Depoloyment', () => {
-        it('Should set the right initial values and owner', async () => {
-            // TODO fill this in! 
-
+        it('Should set the right initial values and owner', async () => { 
+            // Prep
             const {vault, owner} = await loadFixture(deployVaultAMMBaseFixture);
 
+            // Run
+            const asset = await vault.asset();
+            const token0 = await vault.token0();
+            const token1 = await vault.token1();
+            const farmContract = await vault.farmContract();
+            const rewardsToken = await vault.rewardsToken();
+            const isFarmable = await vault.isFarmable();
+            const pid = await vault.pid();
+            const pool = await vault.pool();
+
+            // TODO: Check swap paths
+            // TODO: Check price feeds
+
+            // Test
+            // TODO: Change these to actual values
+            expect(asset).to.equal(zeroAddress);
+            expect(token0).to.equal(zeroAddress);
+            expect(token1).to.equal(zeroAddress);
+            expect(farmContract).to.equal(zeroAddress);
+            expect(rewardsToken).to.equal(zeroAddress);
+            expect(isFarmable).to.equal(true);
+            expect(pid).to.equal(0);
+            expect(pool).to.equal(zeroAddress);
         });
     });
 
     describe('Setters', () => {
-        xit('Should set key tokens and contract addresses', async () => {
-            // TODO
+        it('Should set key tokens and contract addresses', async () => {
+            // Prep
+            const {vault, owner} = await loadFixture(deployVaultAMMBaseFixture);
+            const newAsset = ethers.Wallet.createRandom().address;
+            const newToken0 = ethers.Wallet.createRandom().address;
+            const newToken1 = ethers.Wallet.createRandom().address;
+            const newPool = ethers.Wallet.createRandom().address;
+
+            // Run
+            await vault.setTokens(newAsset, newToken0, newToken1, newPool);            
+
+            // Test
+            // TODO: Change these to actual values
+            expect(await vault.asset()).to.equal(newAsset);
+            expect(await vault.token0()).to.equal(newToken0);
+            expect(await vault.token1()).to.equal(newToken1);
+            expect(await vault.pool()).to.equal(newPool);
         });
 
         xit('Should set farm parameters', async () => {
--- test/vaults/_VaultBase.ts
+++ test/vaults/_VaultBase.ts
@@ -1,18 +1,53 @@
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import {deploymentArgs} from '../../helpers/deployments';
+import { zeroAddress } from "../../helpers/constants";
 
 describe('VaultBase', () => {
     async function deployVaultBaseFixture() {
-        // TODO
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners();
+
+        // Get init arguments for contract deployment
+        const initArgs: any[] = deploymentArgs(owner.address).avax.VaultAMM.TJ_AVAX_USDC;
+
+        // Get contract factory
+        const Vault = await ethers.getContractFactory('TJ_AVAX_USDC');
+        const vault = await upgrades.deployProxy(Vault, initArgs);
+        await vault.deployed();
+
+        return {vault, owner, otherAccount};
     }
 
     describe('Depoloyment', () => {
-        xit('Should set the right initial values and owner', async () => {
-            // TODO
+        it('Should set the right initial values and owner', async () => {
+            // Prep
+            const {vault, owner} = await loadFixture(deployVaultBaseFixture);
 
-            // TODO: Including ERC20 token params
+            // Run
+            const treasury = await vault.treasury();
+            const router = await vault.router();
+            const stablecoin = await vault.stablecoin();
+            const entranceFeeFactor = await vault.entranceFeeFactor();
+            const withdrawFeeFactor = await vault.withdrawFeeFactor();
+            const defaultSlippageFactor = await vault.defaultSlippageFactor();
+            const name = await vault.name();
+            const symbol = await vault.symbol();
+            const decimals = await vault.decimals();
+
+            // Test
+            // TODO: Change these to actual values (not zero)
+            expect(treasury).to.equal(zeroAddress);
+            expect(router).to.equal(zeroAddress);
+            expect(stablecoin).to.equal(zeroAddress);
+            expect(entranceFeeFactor).to.equal(0);
+            expect(withdrawFeeFactor).to.equal(0);
+            expect(defaultSlippageFactor).to.equal(9900);
+            expect(name).to.equal('ZOR LP Vault');
+            expect(symbol).to.equal('ZLPV');
+            expect(decimals).to.equal(18);
         });
     });
 
